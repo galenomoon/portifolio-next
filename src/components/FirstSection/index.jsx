862b5ca6f3c568ab50b7ee843e07f68b2ff64af1
galenomoon <galeno.dev@gmail.com>
2023-06-06 23:23:05 -0300

    responsiveness fixed 100%

--- src/components/FirstSection/index.jsx
+++ src/components/FirstSection/index.jsx
@@ -15,7 +15,7 @@ import { BsGit, BsLinkedin, BsGithub } from 'react-icons/bs'
 import { SiRedux, SiNextdotjs, SiJavascript, SiTailwindcss, SiTypescript, SiRuby } from 'react-icons/si'
 
 export default function FirstSection() {
-  
+
   const container = {
     hidden: { opacity: 1, scale: 0 },
     visible: { opacity: 1, scale: 1, transition: { delayChildren: 0.3, staggerChildren: 0.2 } }
@@ -87,87 +87,84 @@ export default function FirstSection() {
     },
   ]
 
-
   return (
-    <div className='w-full h-screen relative text-center overflow-hidden flex flex-col'>
+    <div className='w-full h-screen max-h-screen relative items-center text-center justify-between overflow-hidden flex flex-col'>
       <NavBar />
-      <section className='sm:mt-[12vh] md:m-0'>
-        <div className='flex flex-col select-none items-center font-bold justify-center text-center text-typography-600 leading-[12vw]'>
-          <motion.p
+      <div className='self-center sm:translate-y-[40px] md:translate-y-0 flex flex-col select-none items-center font-bold justify-center text-center text-typography-600 leading-[12vw]'>
+        <motion.p
+          initial="hidden"
+          animate="visible"
+          className='text-[14.55vw]'
+          variants={{
+            hidden: { opacity: 0 },
+            visible: { opacity: 1, transition: { delay: 0.9 } }
+          }}
+        >
+          FULLSTACK
+        </motion.p>
+        <motion.p
+          initial="hidden"
+          animate="visible"
+          className='text-[14.55vw]'
+          variants={{
+            hidden: { opacity: 0 },
+            visible: { opacity: 1, transition: { delay: 1.3 } }
+          }}
+        >
+          DEVELOPER
+        </motion.p>
+        <div className='w-[90vw] flex text-typography-500 items-center justify-between sm:text-xl md:text-4xl'>
+          <motion.div
             initial="hidden"
             animate="visible"
-            className='text-[14.55vw]'
-            variants={{
-              hidden: { opacity: 0 },
-              visible: { opacity: 1, transition: { delay: 0.9 } }
-            }}
+            className='flex sm:justify-around md:w-auto sm:w-full md:m-0 sm:my-1 items-center gap-2'
+            variants={container}
           >
-            FULLSTACK
-          </motion.p>
-          <motion.p
+            {stacks.map(({ Icon, style, href }, index) =>
+              <motion.a key={index} variants={item} href={href} target='_blank' rel={"norrefer"}>
+                <Icon key={index} className={`${style} duration-200 hover:drop-shadow-xl`} />
+              </motion.a>
+            )}
+          </motion.div>
+          <motion.div
             initial="hidden"
             animate="visible"
-            className='text-[14.55vw]'
-            variants={{
-              hidden: { opacity: 0 },
-              visible: { opacity: 1, transition: { delay: 1.3 } }
-            }}
+            variants={container}
+            className='flex items-baseline gap-2 sm:hidden md:flex'
           >
-            DEVELOPER
-          </motion.p>
-          <div className='w-[90vw] flex text-typography-500 items-center justify-between sm:text-xl md:text-4xl'>
-            <motion.div
-              initial="hidden"
-              animate="visible"
-              className='flex sm:justify-around md:w-auto sm:w-full md:m-0 sm:my-1 items-center gap-2'
-              variants={container}
-            >
-              {stacks.map(({ Icon, style, href }, index) =>
-                <motion.a key={index} variants={item} href={href} target='_blank' rel={"norrefer"}>
-                  <Icon key={index} className={`${style} duration-200 hover:drop-shadow-xl`} />
-                </motion.a>
-              )}
-            </motion.div>
-            <motion.div
-              initial="hidden"
-              animate="visible"
-              variants={container}
-              className='flex items-baseline gap-2 sm:hidden md:flex'
-            >
-              <motion.p variants={item} className='font-normal  md:text-3xl mx-2 leading-[1] self-end'>
-                see more:
-              </motion.p>
-              {socials.map(({Icon, href, style}, index) =>
-               <motion.a key={index} variants={item} href={href} target='_blank' rel={"norrefer"}>
-               <Icon key={index} className={`${style} duration-200 hover:drop-shadow-xl`} />
-             </motion.a>
-              )}
-            </motion.div>
-          </div>
+            <motion.p variants={item} className='font-normal  md:text-3xl mx-2 leading-[1] self-end'>
+              see more:
+            </motion.p>
+            {socials.map(({ Icon, href, style }, index) =>
+              <motion.a key={index} variants={item} href={href} target='_blank' rel={"norrefer"}>
+                <Icon key={index} className={`${style} duration-200 hover:drop-shadow-xl`} />
+              </motion.a>
+            )}
+          </motion.div>
         </div>
-      </section>
-      <motion.section
-        className='w-full absolute sm:top-[265px] md:top-auto md:bottom-10 md:px-16 sm:text-xl'
-        initial="hidden"
-        animate="visible"
-        variants={{
-          hidden: { opacity: 0 },
-          visible: { opacity: 1, transition: { delay: 2 } }
-        }}
-      >
-        <p className='md:text-start sm:text-center text-typography-300'>
+      </div>
+      <div className='w-full py-8'>
+        <motion.p
+          className='w-full sm:translate-y-[40px] md:translate-y-0 sm:top-[265px] md:top-auto md:bottom-10 md:px-16 sm:text-lg md:text-start sm:text-center text-typography-300'
+          initial="hidden"
+          animate="visible"
+          variants={{
+            hidden: { opacity: 0 },
+            visible: { opacity: 1, transition: { delay: 2 } }
+          }}
+        >
           I&apos;m currently focused in improve my <br className='md:block sm:hidden' />
           knowledges, challeging myself and <br className='md:block sm:hidden' />
           exploring the amazing world of technology.
-        </p>
-      </motion.section>
+        </motion.p>
+      </div>
       <motion.div
         initial="hidden"
         animate="visible"
-        className='absolute sm:w-[90%] md:w-[34%] bottom-0 self-center'
+        className='sm:flex md:absolute bottom-0'
         variants={{
           hidden: { y: 120, opacity: 0 },
-          visible: { y: 35, opacity: 1, transition: { delay: 1.5 } }
+          visible: { y: 0, opacity: 1, transition: { delay: 1.5 } }
         }}
       >
         <Image src={galeno} className='object-contain' />
