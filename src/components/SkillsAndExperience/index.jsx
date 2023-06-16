29ebd837466851562661da9964515088ef206d5a
galenomoon <galeno.dev@gmail.com>
2023-06-15 23:46:20 -0300

    improve styles

--- src/components/SkillsAndExperience/index.jsx
+++ src/components/SkillsAndExperience/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Image from 'next/image'
 
 //components
 import SectionTitle from '../SectionTitle'
@@ -6,9 +7,12 @@ import BackgroundText from '../BackgroundText'
 
 //icons
 import { FaLightbulb } from 'react-icons/fa'
-
 import { IoMdColorPalette, IoMdThumbsUp } from 'react-icons/io'
 import { IoExtensionPuzzleSharp, IoPeopleCircle, IoSearch } from 'react-icons/io5'
+
+//assets
+import galeno_img_small from '../../assets/galeno_img_small.png'
+import { BsArrowUpRightCircleFill } from 'react-icons/bs'
 
 export default function SkillsAndExperience() {
 
@@ -31,7 +35,7 @@ export default function SkillsAndExperience() {
     },
     {
       label: 'Good Practices',
-      Icon: () => <IoMdThumbsUp size={30} /> 
+      Icon: () => <IoMdThumbsUp size={30} />
     },
     {
       label: "Problem solving",
@@ -39,12 +43,30 @@ export default function SkillsAndExperience() {
     },
   ]
 
+  const experiences = [
+    {
+      title: 'IM | Mix Institute',
+      subtitle: 'Smartphone software repair teacher',
+      period: '2021',
+    },
+    {
+      title: 'System Developer Couse',
+      subtitle: 'Full Stack Developer',
+      period: '2022',
+    },
+    {
+      title: 'Clear Cloud Software',
+      subtitle: 'Front end and Team Leader - Web development and UI/UX',
+      period: 'now'
+    }
+  ]
+
   return (
-    <section className='text-typography-white-100 py-20 bg-gradient-to-tr from-[#28272C] to-[#28272C] w-full min-h-screen max-h-fit relative items-center text-center justify-between  flex'>
+    <div className='text-typography-white-100 overflow-hidden pt-20 bg-gradient-to-tr from-[#28272C] to-[#28272C] w-full min-h-screen max-h-fit relative items-center text-center justify-between  flex'>
       <BackgroundText white lines={['Skills &', 'Experience']} className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2' />
-      <article className='flex flex-col w-full h-full'>
+      <article className='flex flex-col w-full h-full '>
         <SectionTitle title='Skills & Experience' emoji='👨🏽‍💻' />
-        <section className='flex flex-col w-full h-full text-start text-lg px-28 gap-5 mb-20'>
+        <section className='flex flex-col w-full h-full text-start text-lg px-28 gap-5'>
           <p>
             — 🦋 A creative developer, my passion is solve <br /> problems and improve my self every day.
           </p>
@@ -52,7 +74,7 @@ export default function SkillsAndExperience() {
             — 🎨 Artistic vision and the creativity are ones <br /> of my best qualities.
           </p>
         </section>
-        <section className='flex flex-col w-full h-full text-start text-lg px-28 gap-5'>
+        <section className='flex flex-col w-full h-full text-start text-lg px-28 gap-5 my-20'>
           <h2 className="text-5xl font-semibold">
             Skills
           </h2>
@@ -60,7 +82,7 @@ export default function SkillsAndExperience() {
             {skills?.map(({ label, Icon }, index) =>
               <div key={index} className='flex flex-col w-fit justify-center gap-2'>
                 <div className='text-typography-100 flex items-center justify-center w-[50px] h-[50px] bg-gradient-to-br from-[#A193B9] via-[#8A9EC0] to-[#709F9E] rounded-full'>
-                {Icon ? Icon() : null}
+                  {Icon ? Icon() : null}
                 </div>
                 <span className='text-md'>{label}</span>
               </div>
@@ -68,9 +90,30 @@ export default function SkillsAndExperience() {
           </article>
         </section>
       </article>
-      <article className='flex flex-col w-full h-full'>
-
+      <article className='flex flex-col jusify-between w-full h-fit relative'>
+        <div className="justify-around flex flex-col h-fit ">
+          <h2 className="text-5xl font-semibold mb-10">
+            Experience
+          </h2>
+          {experiences?.map((experience, index) =>
+            <div key={index} className='flex py-3 border-y-2 border-typography-white-300 mx-10'>
+              <div className='w-[60px] px-16 flex items-center justify-center'>
+                <span className='text-lg font-light uppercase'>{experience.period}</span>
+              </div>
+              <div className="flex flex-col text-start justify-center">
+                <p className="text-xl font-semibold">
+                  {experience.title}
+                </p>
+                <span className='text-typography-white-300 text-md'>
+                  {experience.subtitle}
+                </span>
+              </div>
+            </div>
+          )}
+        </div>
+        <section className='flex between w-full h-full text-lg gap-18 mt-30 pb-10 self-end'>
+        </section>
       </article>
-    </section>
+    </div>
   )
 }
