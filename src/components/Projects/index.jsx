d5a9282ce215f87c17b6c764d1d7876c575a8e03
galenomoon <galeno.dev@gmail.com>
2023-08-15 07:00:47 -0300

    adding hi moon as portifolio project

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -11,19 +11,28 @@ import { ScrollContext } from '@/pages';
 //assets
 import lifecell_desktop from '../../assets/projects/lifecell/desktop.png'
 import lifecell_mobile from '../../assets/projects/lifecell/mobile.png'
-// import obraminha_desktop from '../../assets/projects/obraminha/desktop.png'
-// import obraminha_mobile from '../../assets/projects/obraminha/mobile.png'
+import obraminha_desktop from '../../assets/projects/obraminha/desktop.png'
+import obraminha_mobile from '../../assets/projects/obraminha/mobile.png'
+import himoon_desktop from '../../assets/projects/himoon/desktop.png'
+import himoon_mobile from '../../assets/projects/himoon/mobile.png'
 
 export default function Projects() {
   const { ref_projects } = React.useContext(ScrollContext)
 
   const projects = [
-    // {
-    //   title: 'Obraminha',
-    //   description: 'The project boasts a user base exceeding 500, demonstrating its popularity and impact.',
-    //   desktop_image: obraminha_desktop,
-    //   mobile_image: obraminha_mobile
-    // },
+    {
+      title: 'Hi, Moon',
+      description: 'Developed a captivating Landing Page to promote Hi, Moon, a stationery store.',
+      link: 'https://himoonstore.vercel.app/',
+      desktop_image: himoon_desktop,
+      mobile_image: himoon_mobile
+    },
+    {
+      title: 'Obraminha',
+      description: 'The project boasts a user base exceeding 500, demonstrating its popularity and impact.',
+      desktop_image: obraminha_desktop,
+      mobile_image: obraminha_mobile
+    },
     {
       title: 'Life Cell',
       description: 'Developed a captivating Landing Page to promote and sell a Smartphone Repair Course.',
