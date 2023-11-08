f1e1e6ffa10b6197c6983df59b2168af8acb80a4
galenomoon <galeno.dev@gmail.com>
2023-11-08 07:35:29 -0300

    Add meta tags and update page title

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,17 @@
 import { createRef, createContext } from "react";
 
 //components
-import Head from "next/head";
 import Projects from "@/components/Projects";
 import FirstSection from "@/components/Header";
 import ContactMe from "@/components/ContactMe";
 import RepositoryAndFigma from "@/components/RepositoryAndFigma";
 import SkillsAndExperience from "@/components/SkillsAndExperience";
-
-//assets
-import banner from "@/assets/banner.png";
+import Head from "next/head";
 
 //context
 export const ScrollContext = createContext(null);
 
-export default function Home({ title, description, image }) {
+export default function Home() {
   const ref_first_section = createRef(() => {});
   const ref_skills_and_experience = createRef(() => {});
   const ref_repository_and_figma = createRef(() => {});
@@ -38,19 +35,26 @@ export default function Home({ title, description, image }) {
         scrollTo,
       }}
     >
+      <Head>
+        <title> Galenomoon - Fullstack Developer </title>
+        <meta
+          name="og:title"
+          content={
+            "Galenomoon - Fullstack Developer: Showcasing Programming Skills and Expertise"
+          }
+        />
+        <meta
+          name="description"
+          content="Explore the diverse programming skills and expertise of Galenomoon, a fullstack developer specializing in web and application development. Discover a portfolio featuring innovative projects, cutting-edge technologies, and exceptional problem-solving abilities."
+        />
+        <meta
+          name="og:description"
+          content={
+            "Explore the diverse programming skills and expertise of Galenomoon, a fullstack developer specializing in web and application development. Discover a portfolio featuring innovative projects, cutting-edge technologies, and exceptional problem-solving abilities."
+          }
+        />
+      </Head>
       <main className="flex min-h-screen flex-col justify-between bg-background-night-light clash-display font-normal text-typography-100">
-        <Head>
-          <title>{title}</title>
-          <meta name="og:title" content={title} />
-          <meta name="og:image" content={image} />
-          <meta property="og:image:width" content="1200" />
-          <meta property="og:image:height" content="630" />
-          <meta name="og:type" content="website" />
-          <meta name="og:site_name" content="Galenomoon" />
-          <meta name="description" content={description} />
-          <meta name="og:description" content={description} />
-          <meta name="og:url" content="https://www.galenomoon.com" />
-        </Head>
         <FirstSection />
         <SkillsAndExperience />
         <RepositoryAndFigma />
@@ -68,7 +72,6 @@ export async function getStaticProps() {
         "Galenomoon - Fullstack Developer: Showcasing Programming Skills and Expertise",
       description:
         "Explore the diverse programming skills and expertise of Galenomoon, a fullstack developer specializing in web and application development. Discover a portfolio featuring innovative projects, cutting-edge technologies, and exceptional problem-solving abilities.",
-      image: banner,
     },
   };
 }
