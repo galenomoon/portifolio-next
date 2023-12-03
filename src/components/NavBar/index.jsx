ab0968b9e304fbaad95888e60b60b62ca4b9fe86
galenomoon <galeno.dev@gmail.com>
2023-12-03 18:04:10 -0300

    Contele - new skill and job title

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,21 +1,22 @@
-import React from 'react'
+import React from "react";
 
 //styles
-import { motion } from "framer-motion"
-import { IoClose, IoLogoFigma } from 'react-icons/io5';
-import { GiHamburgerMenu } from 'react-icons/gi'
-import { BsGithub, BsLinkedin } from 'react-icons/bs';
+import { motion } from "framer-motion";
+import { IoClose, IoLogoFigma } from "react-icons/io5";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { BsGithub, BsLinkedin } from "react-icons/bs";
 
 //assets
-import Image from 'next/image';
-import logo from '../../assets/logo_light.png';
+import Image from "next/image";
+import logo from "../../assets/logo_light.png";
 
 //context
-import { ScrollContext } from '@/pages';
+import { ScrollContext } from "@/pages";
 
 export default function NavBar() {
-  const [isOpen, setIsOpen] = React.useState(false)
-  const { ref_skills_and_experience, ref_projects, ref_contact, scrollTo } = React.useContext(ScrollContext)
+  const [isOpen, setIsOpen] = React.useState(false);
+  const { ref_skills_and_experience, ref_projects, ref_contact, scrollTo } =
+    React.useContext(ScrollContext);
   const item = { hidden: { y: 20, opacity: 0 }, visible: { y: 0, opacity: 1 } };
 
   const container = {
@@ -23,44 +24,44 @@ export default function NavBar() {
     visible: {
       opacity: 1,
       scale: 1,
-      transition: { delayChildren: 0.3, staggerChildren: 0.2 }
-    }
+      transition: { delayChildren: 0.3, staggerChildren: 0.2 },
+    },
   };
 
   const navbar_options = [
     {
-      name: 'Skills',
+      name: "Skills",
       ref: ref_skills_and_experience,
     },
     {
-      name: 'Portifolio',
+      name: "Portifolio",
       ref: ref_projects,
     },
     {
-      name: 'Contact',
+      name: "Contact",
       ref: ref_contact,
     },
-  ]
+  ];
 
   function mobileScrollTo(ref) {
-    setIsOpen(false)
-    return scrollTo(ref, true)
+    setIsOpen(false);
+    return scrollTo(ref, true);
   }
 
   const socials = [
     {
       Icon: BsGithub,
-      href: 'https://www.github.com/galenomoon',
+      href: "https://www.github.com/galenomoon",
     },
     {
       Icon: BsLinkedin,
-      href: 'https://www.linkedin.com/in/guilherme-galeno-sena/',
+      href: "https://www.linkedin.com/in/guilherme-galeno-sena/",
     },
     {
       Icon: IoLogoFigma,
-      href: 'https://www.figma.com/@galenomoon',
+      href: "https://www.figma.com/@galenomoon",
     },
-  ]
+  ];
 
   return (
     <>
@@ -69,49 +70,74 @@ export default function NavBar() {
         variants={container}
         initial="hidden"
         animate="visible"
-        className='w-full fixed sm:flex z-[300] md:hidden justify-between p-4 h-[90px] items-center  text-typography-400'
+        className="w-full fixed sm:flex z-[300] md:hidden justify-between p-4 h-[90px] items-center  text-typography-400"
       >
         <motion.button
-          whileTap={{ scale: .8 }}
+          whileTap={{ scale: 0.8 }}
           onClick={() => setIsOpen(!isOpen)}
-          className='text-2xl p-3 text-typography-100 bg-white flex items-center justify-center rounded-full z-[101]'
+          className="text-2xl p-3 text-typography-100 bg-white flex items-center justify-center rounded-full z-[101]"
         >
           {isOpen ? <IoClose size={40} /> : <GiHamburgerMenu />}
         </motion.button>
         <motion.article
-          className='flex flex-col items-center top-0 left-0 fixed bg-white justify-center gap-4 text-4xl z-[100] overflow-hidden'
-          initial={{ width: '0px', height: '0px', top: '30px', left: '30px', position: 'absolute' }}
+          className="flex flex-col items-center top-0 left-0 fixed bg-white justify-center gap-4 text-4xl z-[100] overflow-hidden"
+          initial={{
+            width: "0px",
+            height: "0px",
+            top: "30px",
+            left: "30px",
+            position: "absolute",
+          }}
           animate={{
-            width: isOpen ? '100dvw' : '0px',
-            height: isOpen ? '100dvh' : '0px',
-            top: isOpen ? '0px' : '30px',
-            left: isOpen ? '0px' : '30px',
-            borderRadius: isOpen ? '0px' : '25%',
+            width: isOpen ? "100dvw" : "0px",
+            height: isOpen ? "100dvh" : "0px",
+            top: isOpen ? "0px" : "30px",
+            left: isOpen ? "0px" : "30px",
+            borderRadius: isOpen ? "0px" : "25%",
           }}
         >
-          {isOpen &&
+          {isOpen && (
             <>
-              <motion.div variants={container} className='flex flex-col items-center justify-center gap-7'>
-                {navbar_options.map(({ name, ref }, index) =>
-                  <motion.button onClick={() => mobileScrollTo(ref)} key={index} variants={item} className='text-start'>
+              <motion.div
+                variants={container}
+                className="flex flex-col items-center justify-center gap-7"
+              >
+                {navbar_options.map(({ name, ref }, index) => (
+                  <motion.button
+                    onClick={() => mobileScrollTo(ref)}
+                    key={index}
+                    variants={item}
+                    className="text-start"
+                  >
                     {name}
                   </motion.button>
-                )}
+                ))}
               </motion.div>
-              <motion.div variants={container} className='flex w-full h-fit absolute bottom-0 text-white bg-typography-100 items-center justify-between py-8 px-4'>
-                <motion.p variants={item} className='text-md'>
+              <motion.div
+                variants={container}
+                className="flex w-full h-fit absolute bottom-0 text-white bg-typography-100 items-center justify-between py-8 px-4"
+              >
+                <motion.p variants={item} className="text-md">
                   Let&apos;s talk!
                 </motion.p>
-                <motion.div variants={container} className='flex items-center gap-2'>
-                  {socials.map(({Icon, href}, index) =>
-                    <motion.a href={href} key={index} variants={item} target='_blank'>
+                <motion.div
+                  variants={container}
+                  className="flex items-center gap-2"
+                >
+                  {socials.map(({ Icon, href }, index) => (
+                    <motion.a
+                      href={href}
+                      key={index}
+                      variants={item}
+                      target="_blank"
+                    >
                       <Icon />
                     </motion.a>
-                  )}
+                  ))}
                 </motion.div>
               </motion.div>
             </>
-          }
+          )}
         </motion.article>
       </motion.div>
       {/* Desktop */}
@@ -119,34 +145,42 @@ export default function NavBar() {
         variants={container}
         initial="hidden"
         animate="visible"
-        className='w-full sm:hidden md:flex justify-between py-10 px-16 h-[120px] text-typography-300'
+        className="w-full sm:hidden md:flex justify-between py-10 px-16 h-[120px] text-typography-300"
       >
-        <div className='flex w-full justify-between'>
-          <motion.p variants={item} className='text-start'>
-            <Image
-              alt="logo"
-              src={logo}
-              width={200}
-              height={50}
-            />
+        <div className="flex w-full justify-between">
+          <motion.p variants={item} className="text-start">
+            <Image alt="logo" src={logo} width={200} height={50} />
           </motion.p>
-          <motion.p variants={item} className='text-start'>
-            São Paulo,<br />
+          <motion.p variants={item} className="text-start">
+            São Paulo,
+            <br />
             Brazil
           </motion.p>
-          <motion.p variants={item} className='text-start'>
+          <motion.p variants={item} className="text-start">
             Currently Fullstack Developer <br />
-            in Clear Cloud Software
+            in{" "}
+            <a
+              href="https://www.linkedin.com/company/contele/"
+              target="_blank"
+              className="text-typography-100 underline"
+            >
+              Contele Soluções Tecnológicas
+            </a>
           </motion.p>
         </div>
-        <div className='flex w-[70%] justify-end gap-10' >
-          {navbar_options.map(({ name, ref }, index) =>
-            <motion.button onClick={() => scrollTo(ref)} key={index} variants={item} className='text-start'>
+        <div className="flex w-[70%] justify-end gap-10">
+          {navbar_options.map(({ name, ref }, index) => (
+            <motion.button
+              onClick={() => scrollTo(ref)}
+              key={index}
+              variants={item}
+              className="text-start"
+            >
               {name}
             </motion.button>
-          )}
+          ))}
         </div>
       </motion.div>
     </>
-  )
+  );
 }
--- src/components/SkillsAndExperience/index.jsx
+++ src/components/SkillsAndExperience/index.jsx
@@ -10,19 +10,16 @@ import BackgroundText from "../BackgroundText";
 
 //icons
 import { FaLightbulb } from "react-icons/fa";
+import { RiLinkedinBoxFill, RiTeamFill } from "react-icons/ri";
 import { IoMdColorPalette, IoMdThumbsUp } from "react-icons/io";
-import {
-  IoExtensionPuzzleSharp,
-  IoPeopleCircle,
-  IoSearch,
-} from "react-icons/io5";
+import { IoExtensionPuzzleSharp, IoPeopleCircle } from "react-icons/io5";
 
 export default function SkillsAndExperience() {
   const { ref_skills_and_experience } = React.useContext(ScrollContext);
 
   const skills = [
     {
-      label: "Artistic vision",
+      label: "Artistic Vision",
       Icon: () => <IoMdColorPalette size={30} />,
     },
     {
@@ -30,12 +27,12 @@ export default function SkillsAndExperience() {
       Icon: () => <FaLightbulb size={27} />,
     },
     {
-      label: "Leadership",
+      label: "Agile Leadership",
       Icon: () => <IoPeopleCircle size={35} />,
     },
     {
-      label: "Attention to details",
-      Icon: () => <IoSearch size={26} />,
+      label: "Team Communication",
+      Icon: () => <RiTeamFill size={26} />,
     },
     {
       label: "Good Practices",
@@ -62,6 +59,13 @@ export default function SkillsAndExperience() {
     {
       title: "Clear Cloud Software",
       subtitle: "Front-end Developer, Team Leader and UI/UX Designer",
+      linkedin: "https://www.linkedin.com/company/cleardev/",
+      period: "2022 ~ 2023",
+    },
+    {
+      title: "Contele Soluções Tecnológicas",
+      subtitle: "Full Stack Developer",
+      linkedin: "https://www.linkedin.com/company/contele/mycompany/",
       period: "now",
     },
   ];
@@ -92,19 +96,19 @@ export default function SkillsAndExperience() {
             </p>
           </AnimateInView>
         </section>
-        <section className="flex flex-col w-full h-full text-start text-lg sm:px-12 md:px-28 gap-5 my-20">
+        <section className="flex flex-col w-full h-full text-start text-lg sm:px-12 md:px-28 gap-12 my-20">
           <AnimateInView direction="Y" inverse>
             <h2 className="text-5xl font-semibold text-typography-100">
               Skills
             </h2>
           </AnimateInView>
-          <article className="grid md:grid-cols-3 sm:grid-cols-2 gap-3 w-fit">
+          <article className="grid md:grid-cols-3 sm:grid-cols-2 gap-x-6 gap-y-10 w-fit">
             {skills?.map(({ label, Icon }, index) => (
               <AnimateInView
                 key={index}
                 direction="Y"
                 duration={`0.${4 + index}s`}
-                className={"w-auto"}
+                className="w-auto"
               >
                 <div className="flex sm:items-center md:items-start flex-col sm:w-full md:w-fit justify-center gap-2">
                   <div className="text-background-night-dark flex items-center justify-center w-[50px] h-[50px] bg-background-moon rounded-full">
@@ -124,27 +128,44 @@ export default function SkillsAndExperience() {
               Experience
             </h2>
           </AnimateInView>
-          {experiences?.map((experience, index) => (
-            <AnimateInView
-              key={index}
-              duration={`0.${4 + index}s`}
-              className={"w-full"}
-            >
-              <div className="flex py-3 border-y-2 border-typography-white-300 mx-10">
-                <div className="w-[60px] px-16 flex items-center justify-center">
-                  <span className="text-lg font-light uppercase">
-                    {experience.period}
-                  </span>
+          {experiences?.map((experience, index) => {
+            const Element = experience.linkedin ? "a" : "div";
+            return (
+              <AnimateInView
+                key={index}
+                duration={`0.${4 + index}s`}
+                className={"w-full"}
+              >
+                <div className="flex py-3 border-y-2 border-typography-white-300 mx-10">
+                  <div className="w-[60px] px-20 flex items-center justify-center">
+                    <span className="text-lg font-light uppercase whitespace-nowrap">
+                      {experience.period}
+                    </span>
+                  </div>
+                  <div className="flex flex-col text-start justify-center">
+                    <Element
+                      target="_blank"
+                      href={experience.linkedin}
+                      className={`text-xl font-semibold ${
+                        experience.linkedin ? "hover:underline" : ""
+                      }`}
+                    >
+                      {experience.title}
+                      {experience.linkedin ? (
+                        <RiLinkedinBoxFill
+                          className="inline-block ml-2"
+                          size={20}
+                        />
+                      ) : null}
+                    </Element>
+                    <span className="text-typography-white-300 text-md">
+                      {experience.subtitle}
+                    </span>
+                  </div>
                 </div>
-                <div className="flex flex-col text-start justify-center">
-                  <p className="text-xl font-semibold">{experience.title}</p>
-                  <span className="text-typography-white-300 text-md">
-                    {experience.subtitle}
-                  </span>
-                </div>
-              </div>
-            </AnimateInView>
-          ))}
+              </AnimateInView>
+            );
+          })}
         </div>
       </article>
     </div>
